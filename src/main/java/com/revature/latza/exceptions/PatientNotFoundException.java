3b37928e0c23c920a7dd93511c4a102bb384c147
aztal_samoht <thatguysquig@gmail.com>
2021-02-28 18:06:48 -0500

    fixed put patient

---
+++ src/main/java/com/revature/latza/exceptions/PatientNotFoundException.java
@@ -0,0 +1,23 @@
+package com.revature.latza.exceptions;
+
+public class PatientNotFoundException extends RuntimeException {
+	public PatientNotFoundException() {
+	}
+
+	public PatientNotFoundException(String message) {
+		super(message);
+	}
+
+	public PatientNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public PatientNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PatientNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
--- src/main/java/com/revature/latza/services/PatientService.java
+++ src/main/java/com/revature/latza/services/PatientService.java
@@ -1,11 +1,13 @@
 package com.revature.latza.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.latza.exceptions.PatientNotFoundException;
 import com.revature.latza.models.Patient;
 import com.revature.latza.repositories.PatientDAO;
 
@@ -18,7 +20,9 @@ public class PatientService {
 	public List<Patient> findAll() {
 		return pDAO.findAll();
 	}
-	public Patient findByUsername(String unsername) {return null;}
+	public Patient findByUsername(String username) {
+		return pDAO.findByUsername(username).orElseThrow( () -> new PatientNotFoundException("No user found with username " + username));
+	}
 	public Patient findById(Integer id) {return null;}
 	public Patient insert(Patient newPatient) {
 		System.out.println("INFO-entered the insert method of PatientService");
